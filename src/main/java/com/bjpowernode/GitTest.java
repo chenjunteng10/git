a9fa2dd8458afde6bf94e6386889623a1ea6b32d
ten <918660331@qq.com>
2022-08-31 09:39:40 +0800

    second commit 8/31 9:40

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -10,5 +10,6 @@ package com.bjpowernode;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
