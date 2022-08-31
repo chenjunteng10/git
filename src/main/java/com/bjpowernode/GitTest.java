5cd54ffe45e96dee125f08821d3e90d904671f23
ten <918660331@qq.com>
2022-08-31 09:29:03 +0800

    first commit 8/31 9:27

---
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -0,0 +1,14 @@
+package com.bjpowernode;
+
+/**
+ * 描述：
+ *
+ * @author: ten
+ * @date: 2022/8/31 9:23
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+    }
+}
