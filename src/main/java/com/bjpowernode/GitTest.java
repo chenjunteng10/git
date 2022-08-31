dbac84c229cb85b78c4a1268c2d97eaa31e9d9dc
ten <918660331@qq.com>
2022-08-31 12:48:02 +0800

    Revert "hot-fix commit2 8/31 12:45"
    This reverts commit 223c5815af4be63ea308f112ea78c8687b81350c.

--- src/main/java/com/bjpowernode/GitTest.java
+++ src/main/java/com/bjpowernode/GitTest.java
@@ -13,6 +13,5 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master 1");
     }
 }
